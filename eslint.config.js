bdcdecbab0970d2f3b5a5ebbde33eb89327ebe7d
anuj chhikara <anujchhikara777@gmail.com>
2025-12-02 21:44:00 +0530

    Add initial project setup with Expo, including configuration files, basic app structure, and Tailwind CSS integration. Removed unused files and components for a cleaner start.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,17 @@
-// https://docs.expo.dev/guides/using-eslint/
+/* eslint-env node */
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+  prettierConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', '.expo/*', 'build/*'],
+  },
+  {
+    rules: {
+      'react/display-name': 'off',
+    },
   },
 ]);
---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindAttributes: ['className'],
+};
